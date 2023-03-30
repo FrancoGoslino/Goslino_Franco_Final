3ba81dc45516232c1dd67bd314c4b05e59c05959
Franco <frangoslino@gmail.com>
2023-03-30 18:33:29 -0300

    Todo al dia con el ultimo after clase

---
+++ Blog/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from Blog.models import Receta
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+def index(request):
+    return render(request, "Blog/index.html")
+
+
+class RecetaList(ListView):
+    model = Receta
+    context_object_name= "recetas"
+
+class RecetaMineList(LoginRequiredMixin, RecetaList):
+    
+    def get_queryset(self):
+        return Receta.objects.filter(propietario=self.request.user.id).all()
+
+
+
+class RecetaUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Receta
+    success_url = reverse_lazy("receta-list")
+    fields = '__all__'
+
+    def test_func(self):
+        user_id = self.request.user.id
+        receta_id =  self.kwargs.get("pk")
+        return Receta.objects.filter(propietario=user_id, id=receta_id).exists()
+
+class RecetaDetail(DetailView):
+    model=Receta
+
+class RecetaDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Receta
+    context_object_name= "receta"
+    success_url = reverse_lazy("receta-list")
+
+    def test_func(self):
+        user_id = self.request.user.id
+        receta_id =  self.kwargs.get("pk")
+        return Receta.objects.filter(propietario=user_id, id=receta_id).exists()
+
+
+class RecetaCreate(LoginRequiredMixin, CreateView):
+    model = Receta
+    success_url = reverse_lazy("receta-list")
+    fields = [ 'nombre' ,'detalle', 'precio' ,'imagen']
+
+    def form_valid(self, form):
+        form.instance.propietario = self.request.user
+        return super().form_valid(form)
+
+
+
+class RecetaSearch(ListView):
+    model = Receta
+    context_object_name = "recetas"
+
+    def get_queryset(self):
+        criterio = self.request.GET.get("criterio")
+        result = Receta.objects.filter(nombre=criterio).all()
+        return result
+
+class Login(LoginView):
+    next_page = reverse_lazy("receta-list")
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('receta-list')
+
+
+class Logout(LogoutView):
+    template_name = "registration/logout.html"
---
+++ Project/urls.py
@@ -0,0 +1,37 @@
+"""Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Blog.views import index, RecetaList, RecetaCreate, RecetaDelete, RecetaUpdate, RecetaMineList, RecetaDetail, Login, Logout, SignUp
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name= "index/"),
+    path('receta/list', RecetaList.as_view(), name= 'receta-list'),
+    path('receta/list', RecetaMineList.as_view(), name= 'receta-mine'),
+    path('receta/<pk>/update', RecetaUpdate.as_view(), name= 'receta-update'),
+    path('receta/<pk>/delete', RecetaDelete.as_view(), name= 'receta-delete'),
+    path('receta/create', RecetaCreate.as_view(), name= 'receta-create'),
+    path('receta/<pk>/detail', RecetaDetail.as_view(), name= 'receta-detail'),
+    path('login', Login.as_view(), name= 'login'),
+    path('logout', Logout.as_view(), name= 'logout'),
+    path('signup', SignUp.as_view(), name= 'signup'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
